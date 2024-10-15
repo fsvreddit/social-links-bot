2f8ff29c3c602b2e8b12b4c8c5ce50d7e559a063
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2024-10-15 12:53:20 +0100

    Bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@
 
 import { Devvit, TriggerContext, User, UserSocialLink } from "@devvit/public-api";
 import { isLinkId } from "@devvit/shared-types/tid.js";
-import { addDays } from "date-fns";
+import { addDays, addHours } from "date-fns";
 
 enum AppSetting {
     PostId = "postId",
-    AccountName = "accountName",
+    AccountNames = "accountName",
 }
 
 Devvit.addSettings([
@@ -17,12 +17,15 @@ Devvit.addSettings([
         helpText: "Post ID e.g. 1g3l6a6",
     },
     {
-        name: AppSetting.AccountName,
+        name: AppSetting.AccountNames,
         type: "string",
-        label: "Account name to watch for comments from",
+        label: "Account names to watch for comments from",
+        helpText: "Comma separated, not case sensitive",
         defaultValue: "DrRonikBot",
     },
 ]);
+
+const CLEANUP_KEY = "CommentCleanup";
 
 interface UserSocialLinks {
     username: string;
@@ -41,6 +44,7 @@ async function getSocialLinksForUser (username: string, context: TriggerContext)
     }
 
     const socialLinks = await user.getSocialLinks();
+    console.log(`Grabbed ${socialLinks.length} social link(s) for ${username}`);
     return {
         username,
         socialLinks,
@@ -48,22 +52,44 @@ async function getSocialLinksForUser (username: string, context: TriggerContext)
 }
 
 Devvit.addSchedulerJob({
-    name: "deleteCommentAfterOneDay",
+    name: "cleanupJob",
     onRun: async (event, context) => {
-        const commentId = event.data?.commentId as string;
-        if (!commentId) {
+        const commentsToDelete = (await context.redis.zRange(CLEANUP_KEY, 0, new Date().getTime(), { by: "score" })).map(x => x.member);
+        if (commentsToDelete.length === 0) {
             return;
         }
 
-        const comment = await context.reddit.getCommentById(commentId);
-        await comment.delete();
+        for (const commentId of commentsToDelete) {
+            const comment = await context.reddit.getCommentById(commentId);
+            await comment.delete();
+        }
+
+        await context.redis.zRem(CLEANUP_KEY, commentsToDelete);
+    },
+});
+
+Devvit.addTrigger({
+    events: ["AppInstall", "AppUpgrade"],
+    onEvent: async (_, { scheduler }) => {
+        const jobs = await scheduler.listJobs();
+        await Promise.all(jobs.map(job => scheduler.cancelJob(job.id)));
+
+        await scheduler.runJob({
+            name: "cleanupJob",
+            cron: "0 * * * *",
+        });
     },
 });
 
 Devvit.addTrigger({
     event: "CommentCreate",
     onEvent: async (event, context) => {
-        if (!event.comment || !isLinkId(event.comment.parentId)) {
+        if (!event.comment || !event.author) {
+            return;
+        }
+
+        if (!isLinkId(event.comment.parentId)) {
+            console.log("Ignoring non-TLC");
             return;
         }
 
@@ -73,16 +99,26 @@ Devvit.addTrigger({
 
         const settings = await context.settings.getAll();
         const postId = settings[AppSetting.PostId] as string | undefined;
-        const accountName = settings[AppSetting.AccountName] as string | undefined;
-        if (!postId || !accountName) {
+        const accountNamesVal = settings[AppSetting.AccountNames] as string | undefined;
+        if (!postId || !accountNamesVal) {
             return;
         }
 
-        if (event.comment.author !== accountName || event.comment.parentId.endsWith(postId)) {
+        const accountNames = accountNamesVal.split(",").map(accountName => accountName.trim().toLowerCase());
+        console.log(accountNames);
+
+        if (!accountNames.includes(event.author.name.toLowerCase())) {
+            console.log(`Wrong user: Got ${event.author.name}`);
+            return;
+        }
+
+        if (!event.comment.parentId.endsWith(postId)) {
+            console.log("Wrong post.");
             return;
         }
 
         const userList = JSON.parse(event.comment.body) as string[];
+        console.log(userList);
         const userSocialLinks: UserSocialLinks[] = [];
         for (const user of userList) {
             const userSocials = await getSocialLinksForUser(user, context);
@@ -100,6 +136,8 @@ Devvit.addTrigger({
             text: JSON.stringify(userSocialLinks),
         });
 
+        await addCleanup(event.comment.id, context);
+
         await context.scheduler.runJob({
             name: "deleteCommentAfterOneDay",
             runAt: addDays(new Date(), 1),
@@ -108,8 +146,13 @@ Devvit.addTrigger({
     },
 });
 
+export async function addCleanup (commentId: string, context: TriggerContext) {
+    await context.redis.zAdd(CLEANUP_KEY, { member: commentId, score: addHours(new Date(), 3).getTime() });
+}
+
 Devvit.configure({
     redditAPI: true,
+    redis: true,
 });
 
 export default Devvit;
